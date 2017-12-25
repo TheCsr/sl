941728ad171bf95cab188a523574dfff57c6f50e
Chandan Singh <csrrocz@gmail.com>
2017-12-25 16:34:03 +0530

    Add files via upload

--- 2A/2a.py
+++ 2A/2a.py
@@ -1,1 +1,16 @@
+newlist=[]
+def remove_duplicates(numbers):
+	for num in numbers:
+		if num not in newlist:
+			newlist.append(num)
+	return sorted(newlist)
+a=input("Enter the no of elements: ")
+l=[]
 
+for i in range(0 , a):
+	s=input("Enter list of integers:\n")
+	l.append(s)
+print "The entered lisst",l
+
+print "List with removed duplicate element",remove_duplicates(l)
+#print("Final list:\n",final)
