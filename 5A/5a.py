63e6ae7e5b199f1913183d89ff5c2de196ada52c
Chandan Singh <csrrocz@gmail.com>
2017-12-25 16:43:45 +0530

    Add files via upload

--- 5A/5a.py
+++ 5A/5a.py
@@ -1,1 +1,11 @@
 
+list=[]
+a=input('Enter how many numebr you want to enter in the list: ')
+for i in range(0,a):
+	b=input('Here: ')
+	list.append(b)
+m = [x for x in list if x % 2 == 0]
+print("List of even numbers:\n",m)
+y=m[::-1]
+print("Reversed list:\n",y)
+#print(m.reverse())
