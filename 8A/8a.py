3f2719c6f1b5e37242187530cc9b578ecc3a3f0d
Chandan Singh <csrrocz@gmail.com>
2017-12-25 16:49:52 +0530

    Add files via upload

--- 8A/8a.py
+++ 8A/8a.py
@@ -1,1 +1,14 @@
-
+list=[]
+def maximum(list):
+	if len(list)==1:
+		return list[0]
+	else:
+		m = max(list[1:])
+		if m > list[0]:
+			return m
+		else:
+			return list[0]
+	
+list=[10,20,25,100]
+print "The max of the number is: "
+print maximum(list)
