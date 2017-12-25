447e1b9c36a8b31de33badd06adf032958a469a4
Chandan Singh <csrrocz@gmail.com>
2017-12-25 16:37:29 +0530

    Add files via upload

--- 3A/3a.py
+++ 3A/3a.py
@@ -1,1 +1,19 @@
+import sys
+import os
+if(len(sys.argv)!=2):
+	print "Dont be lazy enter the filename.py and filename.txt in the same format!!"
+	sys.exit()
+if(not(os.path.exists(sys.argv[1]))):
+	print "Are you a fool dont you even know that, u need to save the text file first"
+	sys.exit()
+if(sys.argv[1].split('.')[-1]!='txt'):
+	print "Plz give the correct file dimension"
+	sys.exit()
+escape=open(sys.argv[1])
 
+worddic={}
+for line in escape:
+	myline=line.split()
+	for word in myline:
+		worddic[word]=worddic.get(word,0)+1
+print worddic
